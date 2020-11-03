3703db69acb367ac5925f9fd3a550829b7a1a708
camilo23431 <juan.sarria01@usc.edu.co>
2020-11-03 14:33:03 -0500

    Proyecto

---
+++ Park/Models/ViewModels/ListaUsuariosViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Park.Models.ViewModels
+{
+    public class ListaUsuariosViewModels
+    {
+        public int id { get; set; }
+        public string password { set; get; }
+        public string nombre { set; get; }
+        public string fecha { set; get; }
+        public string correo { set; get; }
+        public string tipo_usuario { set; get; }
+        public string telefono { set; get; }
+
+    }
+}
