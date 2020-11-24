8456a8ff23d5426e129489b28f5e7487620b2c40
camilo23431 <juan.sarria01@usc.edu.co>
2020-11-23 23:01:22 -0500

    nuevos cambios

---
+++ Park/Park/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Park.Startup))]
+namespace Park
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
