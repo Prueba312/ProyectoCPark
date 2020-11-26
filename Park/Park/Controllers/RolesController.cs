11886cb3315ccf18f173994d3de8ffae6ccba992
camilo23431 <juan.sarria01@usc.edu.co>
2020-11-26 00:13:52 -0500

    Se agrega funcionalidad de roles

---
+++ Park/Park/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Park.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace Park.Controllers
+{
+    public class RolesController : Controller
+    { 
+        ApplicationDbContext context;
+
+        // GET: Roles
+        public RolesController()
+        {
+            context = new ApplicationDbContext();
+        }
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Create()
+        {
+            var Roles = new IdentityRole();
+            return View(Roles);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Create(IdentityRole Roles)
+        {
+            context.Roles.Add(Roles);
+
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
